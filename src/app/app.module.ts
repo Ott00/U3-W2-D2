9841d34f0ba074972269e72ee0913ba231d9347f
Otman Karim <71219931+Ott00@users.noreply.github.com>
2023-11-28 17:26:54 +0100

    removed unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Route } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -31,7 +30,7 @@ const routes: Route[] = [
     ActivePostsPageComponent,
     InactivePostsPageComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes), FormsModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
